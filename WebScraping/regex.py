db9e8c94a2954ef4abd6662677bc882869a2e94c
Melody Sue <m.melody.sue@gmail.com>
2016-05-23 14:22:20 -0700

    added trial webscraping

---
+++ WebScraping/regex.py
@@ -0,0 +1,14 @@
+import urllib
+import re
+
+def get_quote(symbol):
+	base_url = 'http://finance.google.com/finance?q='
+	content = urllib.urlopen(base_url + symbol).read()
+	m = re.search('id="ref_694653_1".*?>(.*?)<', content)
+	if m:
+		quote = m.group(1)
+	else:
+		quote = 'no quote available for: ' + symbol
+	return quote
+
+print get_quote('goog')
