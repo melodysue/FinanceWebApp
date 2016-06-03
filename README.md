fbb1d5766d021b79b7b07bb55c123cdc7c1f3980
Melody Sue <m.melody.sue@gmail.com>
2016-06-03 12:53:03 -0700

    restarting commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# FinanceWebApp
-Switching to web
--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template, redirect, \
-    url_for, request, session, flash
+    url_for, request, session, flash, g
 from functools import wraps
-from flask.ext.sqlalchemy import SQLAlchemy
+import sqlite3
 
 # create the application object
 app = Flask(__name__)
 
 # config
-import os
-app.config.from_object(os.environ['APP_SETTINGS'])
-
-# create the sqlalchemy object
-db = SQLAlchemy(app)
-
-# import db schema
-from models import *
+app.secret_key = 'my precious'
+app.database = 'sample.db'
 
 
 # login required decorator
@@ -35,7 +29,20 @@ def login_required(f):
 @login_required
 def home():
     # return "Hello, World!"  # return a string
-    posts = db.session.query(BlogPost).all()
+    posts = []
+    try:
+        g.db = connect_db()
+        cur = g.db.execute('select * from posts')
+
+        for row in cur.fetchall():
+            posts.append(dict(title=row[0], description=row[1]))
+
+        # posts = [dict(title=row[0],
+            # description=row[1]) for row in cur.fetchall()]
+
+        g.db.close()
+    except sqlite3.OperationalError:
+        flash('Missing the DB!')
     return render_template('index.html', posts=posts)  # render a template
 
 
@@ -67,6 +74,11 @@ def logout():
     return redirect(url_for('welcome'))
 
 
+# connect to database
+def connect_db():
+    return sqlite3.connect(app.database)
+
+
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
