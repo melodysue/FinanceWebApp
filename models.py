db62c5e899a9bc44bb6309d418439c4c218c2019
Melody Sue <m.melody.sue@gmail.com>
2016-06-03 14:47:18 -0700

    hidding secret key

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from app import db
 
 class BlogPost(db.Model):
+
+	__tablename__ = "posts"
+
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.Integer, nullable=False)
 	description = db.Column(db.Integer, nullable=False)
