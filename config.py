cf7b3d4552844670ad0333f947c8f08ce7d18f90
Melody Sue <m.melody.sue@gmail.com>
2016-06-06 09:54:41 -0700

    fixed database insertion bug

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 # default config
 class BaseConfig(object):
 	DEBUG = False
-	SECRET_KEY = "real_secret_key_hidden"
+	SECRET_KEY = "xac\xa3\x9b;\x10\x96\xaf\x90\x85\x9e\x15\xc15\x92A\x12"
 	db_string = os.environ["DATABASE_URL"]
 	SQLALCHEMY_DATABASE_URI = db_string
 
