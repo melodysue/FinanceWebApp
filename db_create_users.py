6cad3a4208a9228bb2b271893668db32ca19b106
Melody Sue <m.melody.sue@gmail.com>
2016-06-10 16:44:59 -0700

    user authentication compares to database

--- db_create_users.py
+++ db_create_users.py
@@ -2,8 +2,9 @@ from project import db
 from project.models import User
 
 #insert data
-db.session.add(User("Melody","msue16@cmc.edu","Melody88"))
-db.session.add(User("Henry","thehenrysue@gmail.com","thisisme"))
+#db.session.add(User("Melody","msue16@cmc.edu","Melody88"))
+#db.session.add(User("Henry","thehenrysue@gmail.com","thisisme"))
+db.session.add(User("admin","ad@min.com","admin"))
 
 #commit the changes
 db.session.commit()
