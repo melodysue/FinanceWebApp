ba223ca896d6725d4e2beacfbeb67179c99bf8dd
Melody Sue <m.melody.sue@gmail.com>
2016-06-15 14:18:23 -0700

    added user registration form

--- project/home/views.py
+++ project/home/views.py
@@ -2,10 +2,10 @@
 #### imports ####
 #################
 
-from project import app, db
+from project import db
 from project.models import BlogPost
-from flask import flash, redirect, session, url_for, render_template, Blueprint
-from functools import wraps
+from flask import render_template, Blueprint
+from flask_login import login_required
 
 ################
 #### config ####
@@ -15,22 +15,6 @@ home_blueprint = Blueprint(
     'home', __name__,
     template_folder='templates'
 )
-
-
-##########################
-#### helper functions ####
-##########################
-
-
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('users.login'))
-    return wrap
 
 
 ################
--- project/users/views.py
+++ project/users/views.py
@@ -3,9 +3,10 @@
 #################
 
 from flask import flash, redirect, render_template, request, \
-    session, url_for, Blueprint
-from functools import wraps
-from form import LoginForm
+    url_for, Blueprint
+from flask_login import login_user, login_required, logout_user
+from form import LoginForm, RegisterForm
+from project import db
 from project.models import User, bcrypt
 
 ################
@@ -16,21 +17,6 @@ users_blueprint = Blueprint(
     'users', __name__,
     template_folder='templates'
 )
-
-##########################
-#### helper functions ####
-##########################
-
-
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('users.login'))
-    return wrap
 
 
 ################
@@ -49,7 +35,8 @@ def login():
             user = User.query.filter_by(name=request.form['username']).first()
             if user is not None and bcrypt.check_password_hash(
                     user.password, request.form['password']):
-                session['logged_in'] = True
+                #session['logged_in'] = True
+                login_user(user)
                 flash('You were logged in.')
                 return redirect(url_for('home.home'))
                 
@@ -61,6 +48,24 @@ def login():
 @users_blueprint.route('/logout')
 @login_required
 def logout():
-    session.pop('logged_in', None)
+    logout_user()
     flash('You were logged out.')
     return redirect(url_for('home.welcome'))
+
+
+
+@users_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(
+            name=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for('home.home'))
+    return render_template('register.html', form=form)
+    
